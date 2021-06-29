9b8eddcb3b6e001bc3ae32d60522ac2802cdc30c
Minh Trung <nguyenducminhtrung0044@gmail.com>
2021-06-29 15:28:35 +0700

    Add Model

--- mvc/models/BookModel.php
+++ mvc/models/BookModel.php
@@ -8,6 +8,10 @@ class BookModel{
             return "Books";
 
         }
+        public function getNewBook($sl){
+            $sum=$sl*125000;
+            return "Nhớ Em Cô Gái Ngày Hôm Qua : 125.000 VNĐ 2020 : Giá tiền: ".$sum."VNĐ";
+        }
         public function TinhTong($a,$b){
             return $a +$b;
         }
